f8b9d6420656345a4cc9a6e874929aa79b770a9e
gabrielos307 <31230424+gabrielos307@users.noreply.github.com>
2017-09-10 23:13:40 -0500

    Create ayuda.sh

---
+++ ayuda.sh
@@ -0,0 +1,27 @@
+ROJO='\033[0;31m'
+NR='\033[0m'
+
+VER='\033[0;32m'
+NV='\033[0m'
+
+AMA='\033[1;33m'
+NA='\033[0m'
+
+AZ='\033[0;34m'
+NZ='\033[0m'
+
+TUR='\033[0;96m'
+NT='\033[0m'
+
+echo -e"${ROJO}<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>${NR}"
+echo -e"${VER} Bienvenido al menu de ayuda de la prebeshell${NV}"
+echo -e"${AMA} 'arbol'      Muestra archivos y directorios de manera jerarquica  ${NV}"
+echo -e"${AMA} 'ayuda'      Muestra los comando validos${NV}"
+echo -e"${AMA} 'fecha'      Muestra la fecha ${NV}"
+echo -e"${AMA} 'hora'       Muestra la hora${NV}"
+echo -e"${AMA} 'infosis'    Muestra la informacion del sistema${NV}"
+echo -e"${AMA} 'prebeplayer'Abre repoductor de musica ${NV}"
+echo -e"${AMA} 'buscar'     Busca un archivo en la carpeta indicada${NV}"
+echo -e"${AMA} ${NV}"
+
+exit 0
